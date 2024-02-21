b99bce6e9dd8983da1e400a18b17699fd0e1a8ea
hammas <dev.hammas@gmail.com>
2024-02-21 23:33:00 +0500

    add a path helper file in src directory to organize paths in one file

---
+++ src/paths.ts
@@ -0,0 +1,14 @@
+export default {
+  home() {
+    return '/';
+  },
+  topicShow(slug: string) {
+    return `/topics/${slug}`;
+  },
+  postCreate(topicSlug: string) {
+    return `/topics/${topicSlug}/posts/new`;
+  },
+  postShow(topicSlug: string, postId: string) {
+    return `/topics/${topicSlug}/posts/${postId}`;
+  }
+}
