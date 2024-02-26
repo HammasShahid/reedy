f58d88894e0830c5d3e2f426cb0018cbb6ec7c5d
Hammas <hammasshahid11@gmail.com>
2024-02-27 01:46:14 +0500

    separate signIn and signOut server actions to a separate file

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,11 +1,3 @@
 'use server';
 
-import * as auth from '@/auth';
-
-export async function signIn() {
-  return auth.signIn('github');
-}
-
-export async function signOut() {
-  return auth.signOut();
-}
+export { signIn, signOut } from './auth';
