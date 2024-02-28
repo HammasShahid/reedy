7d16413be7c61a62c4c609435f0e370730a1cc6f
Hammas <hammasshahid11@gmail.com>
2024-02-29 00:28:22 +0500

    implement error handling in topic create form and add topic to database

--- src/actions/topic.ts
+++ src/actions/topic.ts
@@ -1,5 +1,63 @@
-'use server'
+'use server';
 
-export async function createTopic() {
-  // TODO: revalidate home page
+import { redirect } from 'next/navigation';
+import { revalidatePath } from 'next/cache';
+import { z } from 'zod';
+import { auth } from '@/auth';
+import { db } from '@/db';
+import { Topic } from '@prisma/client';
+import paths from '@/paths';
+
+interface CreateTopicFormState {
+  errors: {
+    slug?: string[];
+    description?: string[];
+    _form?: string[];
+  };
+}
+
+const createTopicSchema = z.object({
+  slug: z
+    .string()
+    .min(3, 'Slug must be at least 3 characters long')
+    .regex(/[a-z-]+$/, {
+      message: 'Slug must only include alphabets and dashes',
+    }),
+  description: z
+    .string()
+    .min(5, 'Description must be at least 5 characters long'),
+});
+
+export async function createTopic(
+  formState: CreateTopicFormState,
+  formData: FormData
+): Promise<CreateTopicFormState> {
+  const session = await auth();
+  if (!session?.user) {
+    return { errors: { _form: ['You must be logged in to create a topic'] } };
+  }
+
+  const slug = formData.get('slug');
+  const description = formData.get('description');
+
+  const result = createTopicSchema.safeParse({ slug, description });
+
+  if (!result.success) {
+    return { errors: result.error.flatten().fieldErrors };
+  }
+
+  let topic: Topic;
+  try {
+    topic = await db.topic.create({
+      data: { slug: result.data.slug, description: result.data.description },
+    });
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return { errors: { _form: [err.message] } };
+    }
+    return { errors: { _form: ['Something went wrong'] } };
+  }
+
+  revalidatePath(paths.home());
+  redirect(paths.topicShow(topic.slug));
 }
---
+++ src/components/topics/TopicCreateForm.tsx
@@ -0,0 +1,58 @@
+'use client';
+
+import { useFormState } from 'react-dom';
+
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  Button,
+  Input,
+  Textarea,
+} from '@nextui-org/react';
+import { createTopic } from '@/actions/topic';
+
+export default function TopicCreateForm() {
+  const [formState, action] = useFormState(createTopic, { errors: {} });
+  return (
+    <div>
+      <Popover placement="left">
+        <PopoverTrigger>
+          <Button variant="bordered" color="default">
+            Add a topic
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent>
+          <h3 className="font-bold text-lg">Create a topic</h3>
+          <form action={action} className="flex flex-col gap-4 w-80">
+            <Input
+              name="slug"
+              label="Slug"
+              labelPlacement="outside"
+              isInvalid={!!formState.errors.slug}
+              errorMessage={formState.errors.slug?.join(', ')}
+            />
+            <Textarea
+              name="description"
+              label="Description"
+              labelPlacement="outside"
+              placeholder="Topic description"
+              isInvalid={!!formState.errors.description}
+              errorMessage={formState.errors.description?.join(', ')}
+            ></Textarea>
+
+            <Button type="submit" variant="flat" color="primary">
+              Add
+            </Button>
+
+            {formState.errors._form && (
+              <div className="text-red-600 bg-red-100 p-4 rounded">
+                {formState.errors._form.join('\n')}
+              </div>
+            )}
+          </form>
+        </PopoverContent>
+      </Popover>
+    </div>
+  );
+}
