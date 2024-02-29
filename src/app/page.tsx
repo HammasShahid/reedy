615fa161055a2c304cd2a135ed8b50618c73e39b
Hammas <hammasshahid11@gmail.com>
2024-02-29 23:07:43 +0500

    fetch topics list and display on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
+import { Divider } from '@nextui-org/react';
 import TopicCreateForm from '@/components/topics/TopicCreateForm';
+import TopicList from '@/components/topics/TopicList';
 
 export default async function Home() {
   return (
@@ -6,8 +8,11 @@ export default async function Home() {
       <div className="col-span-3">
         <h2 className="2xl font-bold">Top Posts</h2>
       </div>
-      <div className="col-span-1">
+      <div className="col-span-1 flex flex-col gap-4 border shadow py-4 px-2">
         <TopicCreateForm />
+        <Divider />
+        <p className="text-lg">Topics</p>
+        <TopicList />
       </div>
     </div>
   );
