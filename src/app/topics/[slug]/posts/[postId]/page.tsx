6d8b5ddb479fcd97239953c22aa4fb8d2db0bf37
Hammas <hammasshahid11@gmail.com>
2024-03-01 22:47:51 +0500

    show post content on post show page

--- src/app/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -1,5 +1,18 @@
-export default function PostShowPage() {
+import Link from 'next/link';
+import PostShow from '@/components/posts/PostShow';
+import paths from '@/paths';
+
+interface Props {
+  params: { postId: string; slug: string };
+}
+
+export default function PostShowPage({ params: { postId, slug } }: Props) {
   return (
-    <div>PostShowPage</div>
-  )
+    <div className="space-y-9">
+      <Link className="underline decoration-solid" href={paths.topicShow(slug)}>
+        {'< '}Back to {slug}
+      </Link>
+      <PostShow postId={postId} />
+    </div>
+  );
 }
