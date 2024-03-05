0872badca8316a9bda9fde67bb6c5fe8a7ca4ebd
Hammas <hammasshahid11@gmail.com>
2024-03-06 01:19:48 +0500

    use react suspense to show a loading skeleton for post show component

--- src/app/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -1,9 +1,11 @@
+import { Suspense } from 'react';
 import Link from 'next/link';
 import PostShow from '@/components/posts/PostShow';
 import paths from '@/paths';
 import CommentCreateForm from '@/components/comments/CommentCreateForm';
 import CommentList from '@/components/comments/CommentList';
 import { getCommentsByPostId } from '@/db/queries/comment';
+import PostShowLoading from '@/components/posts/PostShowLoading';
 
 interface Props {
   params: { postId: string; slug: string };
@@ -15,7 +17,9 @@ export default function PostShowPage({ params: { postId, slug } }: Props) {
       <Link className="underline decoration-solid" href={paths.topicShow(slug)}>
         {'< '}Back to {slug}
       </Link>
-      <PostShow postId={postId} />
+      <Suspense fallback={<PostShowLoading />}>
+        <PostShow postId={postId} />
+      </Suspense>
       <CommentCreateForm postId={postId} startOpen />
       <CommentList getComments={() => getCommentsByPostId(postId)} />
     </div>
